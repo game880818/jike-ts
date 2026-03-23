10730a3f64793ed697c09824ca35539d23ff0c4d
Syu <game880818@gmail.com>
2026-03-23 21:39:52 +0900

    create router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+// router
+import router from './router/index.tsx'
+import { RouterProvider } from 'react-router-dom'
+
 createRoot(document.getElementById('root')!).render(
-  <App />
+  // routerを導入
+  <RouterProvider router={router} />
 )
