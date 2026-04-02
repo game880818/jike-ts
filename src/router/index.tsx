2c116bccd67af21c88d790d935b72a9225e0a8d5
Syu <game880818@gmail.com>
2026-04-03 04:42:19 +0900

    Add Lazy Loading

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
 import Login from "../pages/Login"
 import Layout from "../pages/Layout";
 import AuthRoute from '@/components/AuthRoute'
 
-import Home from "@/pages/Home";
-import Article from "@/pages/Article";
-import Publish from "@/pages/Publish";
+// import Home from "@/pages/Home";
+// import Article from "@/pages/Article";
+// import Publish from "@/pages/Publish";
+
+const Publish = lazy(() => import('@/pages/Publish'))
+const Article = lazy(() => import('@/pages/Article'))
+const Home = lazy(() => import('@/pages/Home'))
 
 const router = createBrowserRouter([
   {
@@ -20,15 +25,15 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
+        element: <Suspense fallback={'ローディング'}><Home /></Suspense>,
       },
       {
         path: '/article',
-        element: <Article />
+        element: <Suspense fallback={'ローディング'}><Article /></Suspense>,
       },
       {
         path: '/publish',
-        element: <Publish />
+        element: <Suspense fallback={'ローディング'}><Publish /></Suspense>,
       }
     ]
   },
